225ce2d53edb8f2a725327a7cd59a62da375605c
Fable Turas <fable@raintechpdx.com>
2018-02-16 11:33:27 -0800

    pulled from gbr and added packaging files

---
+++ yamlconf/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+copyright (c) 2016-2017 Earth Advantage.
+All rights reserved
+..codeauthor::Paul Munday <paul@paulmunday.net>
+"""
+
+# Local Imports
+from yamlconf.config import Config
+from yamlconf.exceptions import ConfigError
